e116ef4811f9232a92a1a5765456f363ba7e532d
Jai K Robinson <Jai.K.Robinson@student.uts.edu.au>
2017-09-16 13:20:45 +1000

    Added JournalEntry model class

---
+++ Journly/Journly/Models/JournalEntry.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Journly.Models
+{
+    public class JournalEntry
+    {
+        public enum EntryFlag
+        {
+            N, H, D, E
+        }
+
+        public int Id { get; set; }
+        public int JournalId { get; set; }
+        public String Title { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public int Version { get; set; }
+        public String EntryBody { get; set; }
+        public EntryFlag Flag { get; set; }
+        public String EntryEditedReason { get; set; }
+        public int EntryEditedId { get; set; }
+    }
+}
