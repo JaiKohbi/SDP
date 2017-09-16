bc2a19aa5830cc2517e252ff5b50cb96735369db
Jai K Robinson <Jai.K.Robinson@student.uts.edu.au>
2017-09-16 12:44:44 +1000

    Added Journal controller, Journal Model and ViewJournals view.

---
+++ Journly/Journly/Controllers/JournalController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Journly.Models;
+
+namespace Journly.Controllers
+{
+    public class JournalController : Controller
+    {
+        // GET: Journal
+        public ActionResult ViewJournals()
+        {
+            var Journal = new Journal() {Title = "My First Journal"};
+            return View(Journal);
+        }
+    }
+}
---
+++ Journly/Journly/Models/Journal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.EnterpriseServices.Internal;
+using System.Linq;
+using System.Web;
+
+namespace Journly.Models
+{
+    public class Journal
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public String Title { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
