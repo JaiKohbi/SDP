861fd8f7882b1e32f4d22600aefb4bebb6859e27
Jai K Robinson <Jai.K.Robinson@student.uts.edu.au>
2017-10-17 12:58:27 +1100

    Added some Clearer lables to search results page.

--- Journly/Journly/Models/ViewModels/SearchFormModel.cs
+++ Journly/Journly/Models/ViewModels/SearchFormModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -12,11 +13,19 @@ namespace Journly.Models.ViewModels
             Results = new List<JournalEntry>();
             ShowHidden = true;
             ShowDeleted = true;
+            StartDate = DateTime.Today;
+
         }
 
         public String JournalTitle { get; set; }
+
+        [Display(Name = "Search Term")]
         public String SearchString { get; set; }
+
+        [Display(Name = "Start Date OR Single Date")]
         public DateTime StartDate { get; set; }
+
+        [Display(Name = "End Date")]
         public DateTime EndDate { get; set; }
         public int JournalId { get; set; }
         public bool ShowHidden { get; set; }
